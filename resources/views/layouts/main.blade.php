86f07103fbc1ba5e4c3619d6cedf6f29e2c39612
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-13 20:20:21 +0100

    Inserita favicon DC in ogni pagina che estenderà il file main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,9 @@
     <!-- Fontawesome CDN -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Fontawesome CDN -->
+    <!-- Favicon DC -->
+        <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/x-icon">
+    <!-- /Favicon DC -->
     @vite('resources/js/app.js')
 </head>
 <body>
