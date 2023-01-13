669c4df467ee32b40ba780249cae739de2e78fd7
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-13 22:50:30 +0100

    Popolato in modo dinamico i link del footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,61 +1,100 @@
+@php
+    $footer_links_1 = [
+        'title' => 'DC COMICS',
+        'links' => [
+            'Characters',
+            'Comics',
+            'Movies',
+            'TV',
+            'Games',
+            'Videos',
+            'News',
+        ]
+    ];
+
+    $footer_links_2 = [
+        'title' => 'SHOP',
+        'links' => [
+            'Shop DC',
+            'Shop DC Collectibles'
+        ]
+    ];
+
+    $footer_links_3 = [
+        'title' => 'DC',
+        'links' => [
+            'Terms Of Use',
+            'Privacy policy (New)',
+            'Ad Choices',
+            'Advertising',
+            'Jobs',
+            'Subscriptions',
+            'Talent Workshops',
+            'CPSC Certificates',
+            'Ratings',
+            'Shop Help',
+            'Contact Us',
+        ]
+    ];
+
+    $footer_links_4 = [
+        'title' => 'SITES',
+        'links' => [
+            'DC',
+            'MAD Magazine',
+            'DC Kids',
+            'DC Universe',
+            'DC Power Visa'
+        ]
+    ];
+@endphp
+
+
 <footer>
     <div class="footer-top">
         <div class="container py-5">
             <div class="footer-top__menu d-flex">
                 <div class="menu__left me-5">
-                    <h3>DC COMICS</h3>
+                    <h3> {{ $footer_links_1['title'] }}</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
+                            @foreach ($footer_links_1['links'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
                         </ul>
                     </nav>
     
-                    <h3>SHOP</h3>
+                    <h3> {{ $footer_links_2['title'] }}</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC Collectibles</a></li>
+                            @foreach ($footer_links_2['links'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
                         </ul>
                     </nav>
                 </div>
-    
+
                 <div class="menu__center me-5">
-                    <h3>DC</h3>
+                    <h3> {{ $footer_links_3['title'] }}</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Terms Of Use</a></li>
-                            <li><a href="#">Privacy policy (New)</a></li>
-                            <li><a href="#">Ad Choices</a></li>
-                            <li><a href="#">CharAdvertisingacters</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certificates</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
+                            @foreach ($footer_links_3['links'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
                         </ul>
                     </nav>
                 </div>
-    
+
                 <div class="menu__right me-5">
-                    <h3>SITES</h3>
+                    <h3>{{ $footer_links_4['title'] }}</h4>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">MAD Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
+                            @foreach ($footer_links_4['links'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
                         </ul>
                     </nav>
-                </div>
+                </div> 
             </div>
     
             <small class="d-block">All Site Content TM and C 2020 DC Entertainment, unless otherwise <span class="em-phrase"><a href="#" class="text-decoration-none">noted here</a></span>. All rights reserved.</small>
