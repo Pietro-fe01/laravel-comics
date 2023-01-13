f336f07b0f9810d300827ff8e9b2c0729bfbc334
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-13 18:36:16 +0100

    Modifiche scss: - Aggiunto effetto all'hover dei link del menù top - Popolato le voci del menù con i link reali

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,20 +7,20 @@
                     <nav>
                         <ul class="p-0">
                             <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
                         </ul>
                     </nav>
     
                     <h3>SHOP</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -29,17 +29,17 @@
                     <h3>DC</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">CharAdvertisingacters</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -48,11 +48,11 @@
                     <h3>SITES</h3>
                     <nav>
                         <ul class="p-0">
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
                         </ul>
                     </nav>
                 </div>
