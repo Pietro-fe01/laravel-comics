76118a251a8a24e455c04cffa702e6eafe1402fa
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-13 20:05:55 +0100

    Inserito badge "CURRENT SERIE" tra la hero e la prima sezione del main

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -9,7 +9,6 @@
         <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
     </section>
 
-    {{-- @dd($comics_database); --}}
     <section class="comics-magazines container">
         @foreach ($comics_database as $comic)
         <!-- Singola CARD magazine -->
@@ -27,5 +26,44 @@
         @endforeach
 
         <button class="comics-load-more-btn mb-4">LOAD MORE</button>
+    </section>
+
+    <section class="comics-pre-footer">
+        <div class="comics-cards container d-flex justify-content-between align-items-center">
+            <div class="comics-card ms-5">
+                <a href="#" class="comics-card__info">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+
+            <div class="comics-card">
+                <a href="#" class="comics-card__info">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+
+            <div class="comics-card">
+                <a href="#" class="comics-card__info">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+
+            <div class="comics-card">
+                <a href="#" class="comics-card__info">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+
+            <div class="comics-card me-5">
+                <a href="#" class="comics-card__info">
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+        </div>
     </section>
 @endsection
