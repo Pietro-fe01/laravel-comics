9ae7156703af185eb4d764bc2234a2af2a0b9d10
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-13 16:37:32 +0100

    Modificato il titolo della pagina comics e characters: - Viene stampato la scritta DC | (standard) più il titolo della sezione

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
+@section('page-title')
+    Characters
+@endsection
+
 @section('main-content')
 
 @endsection
--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
+@section('page-title')
+    Comics
+@endsection
+
 @section('main-content')
 
 @endsection
